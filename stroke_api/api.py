a5a6266c7dd5975efa66340e8267bf76af678709
emese007 <emese.hofi@gmail.com>
2025-08-08 13:59:44 +0200

    push

--- stroke_api/api.py
+++ stroke_api/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from stroke_api import filters
+from stroke_api.filters import filter_patient
 
 router = APIRouter()
 
@@ -7,15 +8,21 @@ router = APIRouter()
 def read_root():
     return {"message": "Bienvenue sur l'API Stroke Prediction !"}
 
-# TODO décommenter et compléter
-#@router.get("/patients/")
-#def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
-    #filtered_df = 
-    #return filtered_df.to_dict(orient="records")
+@router.get("/patients/")
+def get_patients(gender: str = None, stroke: int = None, max_age: float = None):
+    result = filters.filter_patient(gender, stroke, max_age)
+    return result
 
 # TODO décommenter et compléter
-#@router.get("/patients/{patient_id}")
+@router.get("/patients/{patient_id}")
+def get_patients_by_id(patient_id: int = None):
+    result = filters.get_patients_by_id(patient_id)
+    return result
     # Gérer le cas où l'id de patient passé en paramètre n'existe pas
-
+    if patient_id.empty:
+        
+        raise HTTPException(404,"Patient ID not found")
+        else:
+        return patient.to_dict('records')
 
 # TODO Ajout de la route stats
